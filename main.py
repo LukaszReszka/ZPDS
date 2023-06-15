cd2f2bac3a527efb9373320a661121e6d958688b
LukaszReszka <01143598@pw.edu.pl>
2023-06-15 22:13:34 +0200

    First steps in streamlit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Beerify',
+    page_icon="🍻",
+    layout="wide"
+)
+
+with st.sidebar:
+    st.image('assets\logo.png', width=256)
+    st.title('Beerify')
+    st.divider()
+st.title('Beerify')
